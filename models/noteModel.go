b05d3be678e145ee557cc70a2d0f88be4ea73826
needrima <oyebodeamirdeen@gmail.com>
2022-04-12 05:16:20 -0700

    completed all models

--- models/noteModel.go
+++ models/noteModel.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Note struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	Text      string             `json:"text"`
+	CreatedAt time.Time          `json:"created _at"`
+	UpdatedAt time.Time          `json:"updated_at"`
+	NoteId    string             `json:"note_id"`
+}
