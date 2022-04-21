0e21ed9022090c08831818ed26e3c21efec039cb
needrima <oyebodeamirdeen@gmail.com>
2022-04-21 01:39:49 -0700

    completed  invoice cotrollers

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -6,9 +6,11 @@ import (
 	"net/http"
 	"restaurant-management-API/database"
 	"restaurant-management-API/models"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func GetInvoices() gin.HandlerFunc {
@@ -52,18 +54,110 @@ func GetInvoice() gin.HandlerFunc {
 			return
 		}
 
-		ctx.JSON(http.StatusOK, invoice)
+		var invoiceView models.InvoiceView
+
+		allOrderItems, err := ItemsByOrder(invoice.OrderId)
+		if err != nil {
+			log.Println("error getting all order items:", err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "something went wrong",
+			})
+			return
+		}
+
+		invoiceView.OrderId = invoice.OrderId
+		invoiceView.PaymentDueDate = invoice.PaymentDueDate
+		invoiceView.PaymentMethod = invoice.PaymentMethod
+		invoiceView.InvoiceId = invoice.InvoiceId
+		invoiceView.PaymentStatus = invoice.PaymentStatus
+		invoiceView.PaymentDue = allOrderItems[0]["payment_due"]
+		invoiceView.TableNumber = allOrderItems[0]["table_number"]
+		invoiceView.OrderDetails = allOrderItems[0]["order_items"]
+
+		ctx.JSON(http.StatusOK, invoiceView)
 	}
 }
 
 func CreateInvoice() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var invoice models.Invoice
+		if err := ctx.BindJSON(&invoice); err != nil {
+			log.Println("error getting all invoice from frontend:", err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "something went wrong",
+			})
+			return
+		}
 
+		if invoice.PaymentStatus == "" {
+			invoice.PaymentStatus = "PENDING"
+		}
+
+		var order models.Order
+		orderCollection := database.GetCollection("order")
+		if err := orderCollection.FindOne(context.TODO(), bson.M{"order_id": invoice.OrderId}).Decode(&order); err != nil {
+			log.Println("invalid orderId:", err)
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "invalid order id, check order id and try again",
+			})
+			return
+		}
+
+		invoice.PaymentDueDate, _ = time.Parse(time.ANSIC, time.Now().AddDate(0, 0, 1).Format(time.ANSIC))
+		invoice.CreatedAt, _ = time.Parse(time.ANSIC, time.Now().Format(time.ANSIC))
+		invoice.UpdatedAt, _ = time.Parse(time.ANSIC, time.Now().Format(time.ANSIC))
+
+		invoice.ID = primitive.NewObjectID()
+		invoice.InvoiceId = invoice.ID.Hex()
+
+		if err := validate.Struct(invoice); err != nil {
+			log.Println("invalid invoice:", err)
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "something went wrong",
+			})
+			return
+		}
+
+		invoiceCollection := database.GetCollection("invoice")
+		insertResult, err := invoiceCollection.InsertOne(context.TODO(), invoice)
+		if err != nil {
+			log.Println("invalid invoice:", err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "something went wrong",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, insertResult)
 	}
 }
 
 func UpdateInvoice() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		invoiceId := ctx.Param("invoice_id")
+		var invoice models.Invoice
+		if err := ctx.BindJSON(&invoice); err != nil {
+			log.Println("error getting all update invoice from frontend:", err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "something went wrong",
+			})
+			return
+		}
 
+		if invoice.PaymentStatus == "" {
+			invoice.PaymentStatus = "PENDING"
+		}
+
+		invoiceCollection := database.GetCollection("invoice")
+		updateResult, err := invoiceCollection.UpdateOne(context.TODO(), bson.M{"invoice_id": invoiceId}, bson.M{"$set": invoice})
+		if err != nil {
+			log.Println("error updating invoice:", err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "could not update invoice",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, updateResult)
 	}
 }
--- models/invoiceModel.go
+++ models/invoiceModel.go
@@ -10,9 +10,20 @@ type Invoice struct {
 	ID             primitive.ObjectID `bson:"_id"`
 	InvoiceId      string             `json:"invoice_id"`
 	OrderId        string             `json:"order_id"`
-	PaymentMethod  string             `json:"payment_method" validate:"eq=CARD|eq=CASH|eq="`
-	PaymentStatus  string             `json:"payment_status" validate:"required,eq=PENDING|eq=PAID"`
-	PaymentDueDate time.Time          `json:"payment_due_date"`
+	PaymentMethod  string             `json:"payment_method" bson:"payment_method,omitempty" validate:"eq=CARD|eq=CASH|eq="`
+	PaymentStatus  string             `json:"payment_status" bson:"payment_status,omitempty" validate:"required,eq=PENDING|eq=PAID"`
+	PaymentDueDate time.Time          `json:"payment_due_date" bson:"payment_due_date,omitempty"`
 	CreatedAt      time.Time          `json:"created_at"`
 	UpdatedAt      time.Time          `json:"updated_at"`
+}
+
+type InvoiceView struct {
+	InvoiceId      string      `json:"invoice_id,omitempty"`
+	PaymentMethod  string      `json:"payment_method,omitempty"`
+	OrderId        string      `json:"order_id,omitempty"`
+	PaymentStatus  string      `json:"payment_status,omitempty"`
+	PaymentDue     interface{} `json:"payment_due,omitempty"`
+	TableNumber    interface{} `json:"table_number,omitempty"`
+	PaymentDueDate time.Time   `json:"payment_due_date,omitempty"`
+	OrderDetails   interface{} `json:"order_details,omitempty"`
 }
